4b6b1c9c25c5761588b8ca390089864e3e949f99
Mohammed Ihsaan <mohammedihsaan28@gmail.com>
2026-01-09 20:21:53 +0530

    Update thr trpc provider

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -1,27 +1,25 @@
-'use client';
+"use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { httpBatchLink } from "@trpc/client";
 import { PropsWithChildren, useState } from "react";
-import {trpc} from '@/app/_trpc/client'
+import { trpc } from "@/app/_trpc/client";
 
-export default function Providers({children}: PropsWithChildren) {
+export default function Providers({ children }: PropsWithChildren) {
+  const [queryClient] = useState(() => new QueryClient());
+  const [trpcClient] = useState(() =>
+    trpc.createClient({
+      links: [
+        httpBatchLink({
+          url: "https://git-view-2jkd.vercel.app/api/trpc",
+        }),
+      ],
+    })
+  );
 
-    const [queryClient] = useState(()=> new QueryClient());
-    const [trpcClient] = useState(()=> trpc.createClient({
-        links:[
-            httpBatchLink({
-                url:'http://localhost:3000/api/trpc',
-            })
-        ]
-    }))
-
-
-    return(
-        <trpc.Provider client={trpcClient} queryClient={queryClient} >
-            <QueryClientProvider client={queryClient}>
-                {children}
-            </QueryClientProvider>
-        </trpc.Provider>
-    )
+  return (
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </trpc.Provider>
+  );
 }
