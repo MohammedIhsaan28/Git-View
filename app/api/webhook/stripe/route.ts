7c4dc0c8d061c6607c5257c38a37721dbd0b0f6c
Mohammed Ihsaan <mohammedihsaan28@gmail.com>
2026-01-09 21:29:10 +0530

    Fix the stripe

--- app/api/webhook/stripe/route.ts
+++ app/api/webhook/stripe/route.ts
@@ -1,51 +1,64 @@
 import db from "@/lib/db";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
-import Stripe from 'stripe';
+import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!,{
-    apiVersion:'2025-12-15.clover'
-})
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: "2025-12-15.clover",
+});
 
-export async function POST(req:Request){
-    const body = await req.text();
-    const signature = (await headers()).get('stripe-signature') as string;
+export async function POST(req: Request) {
+  const body = await req.text();
+  const signature = req.headers.get("stripe-signature");
 
-    let event: Stripe.Event;
+  let event: Stripe.Event;
 
-    try{
-        event = stripe.webhooks.constructEvent(body, signature, process.env.STRIPE_WEBHOOK_SECRET!);
-
-    } catch(err){
-        return NextResponse.json({ error: 'Invalid signature' },{ status: 400});
-    }
+  try {
+    event = stripe.webhooks.constructEvent(
+      body,
+      signature!,
+      process.env.STRIPE_WEBHOOK_SECRET!
+    );
     const session = event.data.object as Stripe.Checkout.Session;
     console.log(event.type);
 
-    if(event.type === 'checkout.session.completed'){
-        const credits = Number(session.metadata?.['credits']);
-        const userId = session.client_reference_id;
-        if(!userId || !credits){
-            return NextResponse.json({ error: 'Missing userId or credits'},{ status: 400});
-        }
-        await db.stripeTransaction.create({
-            data:{
-                userId,
-                credits
-            }
-        })
+    if (event.type === "checkout.session.completed") {
+      const credits = Number(session.metadata?.["credits"]);
+      const userId = session.client_reference_id;
+      if (!userId || !credits) {
+        return NextResponse.json(
+          { error: "Missing userId or credits" },
+          { status: 400 }
+        );
+      }
+      await db.stripeTransaction.create({
+        data: {
+          userId,
+          credits,
+        },
+      });
 
-        await db.user.update({
-            where:{
-                id: userId
-            },
-            data:{
-                credits:{
-                    increment: credits
-                }
-            }
-        })
-        return NextResponse.json({ message: 'Credits added successfully'},{ status: 200});
+      await db.user.update({
+        where: {
+          id: userId,
+        },
+        data: {
+          credits: {
+            increment: credits,
+          },
+        },
+      });
+      return NextResponse.json(
+        { message: "Credits added successfully" },
+        { status: 200 }
+      );
     }
-    
+  } catch (err) {
+    return NextResponse.json(
+      { error: `Invalid signature ${err}` },
+      { status: 400 }
+    );
+  }
+
+    return NextResponse.json({ success: true }, { status: 200 });
 }
